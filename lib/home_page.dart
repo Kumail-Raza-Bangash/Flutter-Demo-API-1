7994a622f73a1d33b1cbc30f89c2617dd2667d81
Kumail Raza <kumailraza260@gmail.com>
2023-09-26 21:49:25 +0500

    Update1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,20 +1,72 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomePage extends StatefulWidget {  
+import 'package:flutter/material.dart';
+import 'package:flutter_demo_api_1/model.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Welcome> welcome = [];
+
   @override  
   Widget build(BuildContext context) {  
-    return Scaffold(  
-      appBar: AppBar(  
-          title:const Text("FLutter Demo API 1")  
-      ),  
-      body: const Center(  
-          child:Text("Welcome to Flutter API")  
-      ),  
+    return FutureBuilder(
+      future: getData(),
+      builder: (context, snapshot) {
+        if(snapshot.hasData){
+
+          return ListView.builder(
+          itemCount: welcome.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 130,
+              color: Colors.greenAccent,
+              padding: const EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('user id: ${welcome[index].userId}', style: const TextStyle(fontSize: 14, color: Colors.red, fontWeight: FontWeight.bold,),),
+                  Text('id:  ${welcome[index].id} ', style: const TextStyle(fontSize: 14, color: Colors.red, fontWeight: FontWeight.bold,),),
+                  Text('titile:  ${welcome[index].title}',maxLines: 1, style: const TextStyle(fontSize: 14, color: Colors.red),),
+                  Text('body:  ${welcome[index].body} ',maxLines: 1, style: const TextStyle(fontSize: 14, color: Colors.red),),
+                ],
+              ),
+            );
+          }
+        );
+
+        }
+        else{
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        
+      }
     );  
   }  
+
+  Future<List<Welcome>> getData() async{
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+
+    if(response.statusCode == 200){
+      for(Map<String, dynamic> index in data){
+        welcome.add(Welcome.fromJson(index));
+      }
+      return welcome;
+
+    }
+    else{
+      return welcome;
+    }
+  } 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,40 @@ void main() {
   runApp( MyApp());
 }
   
-class MyApp extends StatelessWidget {  
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override  
   Widget build(BuildContext context) {  
     return MaterialApp(  
         theme: ThemeData(  
           primarySwatch: Colors.green,  
         ),  
-        home: HomePage()  
+        home: MyHomePage()  
     );  
   }  
 }  
- 
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override  
+  Widget build(BuildContext context) {  
+    return Scaffold(  
+      appBar: AppBar(  
+          title:const Text("FLutter Demo API 1")  
+      ),  
+      body: HomePage(),  
+    );  
+  }  
+}
